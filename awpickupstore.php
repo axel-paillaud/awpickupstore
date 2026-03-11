2ae9dac28751c042b140ce833a84f691a3a15ccd
Axel Paillaud <contact@axelweb.fr>
2026-03-11 09:13:19 +0100

    init main module file and hook

--- awpickupstore.php
+++ awpickupstore.php
@@ -19,7 +19,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    Axelweb <contact@axelweb.fr>
- * @copyright 2007-2024 Axelweb
+ * @copyright 2026 Axelweb
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of Axelweb
  */
@@ -29,24 +29,17 @@ if (!defined('_PS_VERSION_')) {
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-class AwModuleBase extends Module
+use Axelweb\AwPickupStore\Repository\PickupStoreRepository;
+
+class AwPickupStore extends Module
 {
-    public $tabs = [
-        [
-            'name' => [
-                'en' => 'Module Base',
-                'fr' => 'Module Base',
-            ],
-            'class_name' => 'AwModuleBase',
-            'parent_class_name' => 'AdminParentModulesSf',
-            'wording' => 'Module Base',
-            'wording_domain' => 'Modules.Awmodulebase.Admin',
-        ],
-    ];
+    private PickupStoreRepository $repository;
 
     public function __construct()
     {
-        $this->name = 'awmodulebase';
+        $this->repository = new PickupStoreRepository();
+
+        $this->name = 'awpickupstore';
         $this->tab = 'administration';
         $this->version = '1.0.0';
         $this->author = 'Axelweb';
@@ -56,10 +49,10 @@ class AwModuleBase extends Module
 
         parent::__construct();
 
-        $this->displayName = $this->trans('Module Base', [], 'Modules.Awmodulebase.Admin');
-        $this->description = $this->trans('Base template module for PrestaShop development', [], 'Modules.Awmodulebase.Admin');
+        $this->displayName = $this->trans('Pickup Store', [], 'Modules.Awpickupstore.Admin');
+        $this->description = $this->trans('Prestashop module to add temporary in-store pickup options and specify an appointment date.', [], 'Modules.Awpickupstore.Admin');
 
-        $this->confirmUninstall = $this->trans('Are you sure you want to uninstall this module?', [], 'Modules.Awmodulebase.Admin');
+        $this->confirmUninstall = $this->trans('Are you sure you want to uninstall this module?', [], 'Modules.Awpickupstore.Admin');
 
         $this->ps_versions_compliancy = [
             'min' => '8.0',
@@ -81,7 +74,10 @@ class AwModuleBase extends Module
         $installed = parent::install()
             && $this->installDb()
             && $this->registerHook('actionFrontControllerSetMedia')
-            && Configuration::updateValue('AWMODULEBASE_SAMPLE_CONFIG', '');
+            && $this->registerHook('displayCarrierExtraContent')
+            && $this->registerHook('actionCarrierProcess')
+            && $this->registerHook('actionValidateOrder')
+            && $this->registerHook('displayAdminOrderMain');
 
         // Prevent 'Unable to generate a URL for the named route [...]' error,
         // clear Symfony cache
@@ -95,7 +91,6 @@ class AwModuleBase extends Module
     public function uninstall(): bool
     {
         return parent::uninstall()
-            && Configuration::deleteByName('AWMODULEBASE_SAMPLE_CONFIG')
             && $this->uninstallDb();
     }
 
@@ -120,31 +115,125 @@ class AwModuleBase extends Module
      */
     public function getContent(): void
     {
-        $route = $this->get('router')->generate('awmodulebase_form_configuration');
+        $route = $this->get('router')->generate('awpickupstore_form_configuration');
         Tools::redirectAdmin($route);
     }
 
     /**
-     * Hook to register CSS and JS on front-office pages
+     * Register CSS and JS on the checkout page only
      */
-    public function hookActionFrontControllerSetMedia()
+    public function hookActionFrontControllerSetMedia(): void
     {
+        if ($this->context->controller->php_self !== 'order') {
+            return;
+        }
+
         $this->context->controller->registerStylesheet(
-            'module-awmodulebase-style',
-            'modules/' . $this->name . '/views/css/awmodulebase.css',
-            [
-                'media' => 'all',
-                'priority' => 200,
-            ]
+            'module-awpickupstore-style',
+            'modules/' . $this->name . '/views/css/awpickupstore.css',
+            ['media' => 'all', 'priority' => 200]
         );
 
         $this->context->controller->registerJavascript(
-            'module-awmodulebase-script',
-            'modules/' . $this->name . '/views/js/awmodulebase.js',
-            [
-                'position' => 'bottom',
-                'priority' => 200,
-            ]
+            'module-awpickupstore-script',
+            'modules/' . $this->name . '/views/js/awpickupstore.js',
+            ['position' => 'bottom', 'priority' => 200]
         );
+    }
+
+    /**
+     * Display message and/or appointment picker when a carrier is selected at checkout
+     */
+    public function hookDisplayCarrierExtraContent(array $params): string
+    {
+        $idCarrier = (int) ($params['carrier']['id'] ?? 0);
+        if (!$idCarrier) {
+            return '';
+        }
+
+        $config = $this->repository->getCarrierConfig($idCarrier);
+
+        if (!$config) {
+            return '';
+        }
+
+        $this->context->smarty->assign([
+            'awpickupstore_message'             => $config['message'],
+            'awpickupstore_require_appointment' => (bool) $config['require_appointment'],
+            'awpickupstore_id_carrier'          => $idCarrier,
+        ]);
+
+        return $this->display(__FILE__, 'views/templates/hook/carrier_extra_content.tpl');
+    }
+
+    /**
+     * Validate appointment and store it by id_cart during checkout carrier step
+     */
+    public function hookActionCarrierProcess(array $params): void
+    {
+        $idCarrier = (int) ($params['cart']->id_carrier ?? 0);
+        if (!$idCarrier) {
+            return;
+        }
+
+        $config = $this->repository->getCarrierConfig($idCarrier);
+
+        if (!$config || !$config['require_appointment']) {
+            return;
+        }
+
+        $date = Tools::getValue('awpickupstore_date');
+        $time = Tools::getValue('awpickupstore_time');
+
+        if (empty($date) || empty($time)) {
+            $this->context->controller->errors[] = $this->trans(
+                'Please select an appointment date and time.',
+                [],
+                'Modules.Awpickupstore.Shop'
+            );
+
+            return;
+        }
+
+        $datetime = date('Y-m-d H:i:s', strtotime($date . ' ' . $time));
+        $idCart   = (int) $params['cart']->id;
+
+        $this->repository->upsertAppointment($idCart, $datetime);
+    }
+
+    /**
+     * Attach appointment to the order once it is created
+     */
+    public function hookActionValidateOrder(array $params): void
+    {
+        $idCart  = (int) ($params['cart']->id ?? 0);
+        $idOrder = (int) ($params['order']->id ?? 0);
+
+        if (!$idCart || !$idOrder) {
+            return;
+        }
+
+        $this->repository->attachOrderToAppointment($idCart, $idOrder);
+    }
+
+    /**
+     * Display appointment info in the BO order detail page
+     */
+    public function hookDisplayAdminOrderMain(array $params): string
+    {
+        $idOrder = (int) ($params['id_order'] ?? 0);
+        if (!$idOrder) {
+            return '';
+        }
+
+        $datetime = $this->repository->getAppointmentByOrder($idOrder);
+
+        if (!$datetime) {
+            return '';
+        }
+
+        $this->context->smarty->assign('awpickupstore_appointment_datetime', $datetime);
+
+        return $this->display(__FILE__, 'views/templates/hook/admin_order.tpl');
     }
 }
